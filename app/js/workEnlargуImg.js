9ae7fc6d91e821a0e203f4c46e1898a6196df67a
Roman Samolazov <samolazov.roman.alex@gmail.com>
2021-02-10 10:32:57 +0300

    Layot a block work

--- app/js/workEnlargуImg.js
+++ app/js/workEnlargуImg.js
@@ -2,16 +2,20 @@ const works = document.querySelector('.works');
 const worksBoxImg= document.querySelectorAll('.works-box__img');
 const worksImg= document.querySelector('.works__img');
 
-worksBoxImg.forEach(img=>{
-    img.addEventListener('click',()=>{
-        if (img.classList.length!=3){
-            img.classList.remove('w-20');
-            img.classList.add('w-50');
-            img.classList.add('absolute');
-        }else{
-            img.classList.remove('w-50');
-            img.classList.add('w-20');
-            img.classList.remove('absolute');
-        }
-    })
-})
+
+
+
+
+// worksBoxImg.forEach(img=>{
+//     img.addEventListener('click',()=>{
+//         if (img.classList.length!=3){
+//             img.classList.remove('w-20');
+//             img.classList.add('w-50');
+//             img.classList.add('absolute');
+//         }else{
+//             img.classList.remove('w-50');
+//             img.classList.add('w-20');
+//             img.classList.remove('absolute');
+//         }
+//     })
+// })
